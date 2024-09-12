0c1ae0dece8121757eb43460d3d1ee7ef34e9bb8
naflexsil <starrikovaalina@gmail.com>
2024-09-12 20:01:42 +0700

    first commit

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MainTest {
-  
+    @Test
+    public void testFizzBuzz() {
+        // должны вернуть "fizzbuzz"
+        assertEquals("fizzbuzz", Main.fizzBuzz(35));
+        // должны вернуть "fizz"
+        assertEquals("fizz", Main.fizzBuzz(10));
+
+        // должны вернуть "buzz"
+        assertEquals("buzz", Main.fizzBuzz(14));
+
+        // должны вернуть само число
+        assertEquals("1", Main.fizzBuzz(1));
+    }
 }
